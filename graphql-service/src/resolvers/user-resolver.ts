67867892c0e518c740515651d8cc39853bb76952
Andrew S. Gardner <asg@andrewsgardner.com>
2020-03-30 20:53:56 -0400

    Fix user() method

--- graphql-service/src/resolvers/user-resolver.ts
+++ graphql-service/src/resolvers/user-resolver.ts
@@ -1,16 +1,28 @@
-import { ObjectId } from 'mongodb';
-import { Resolver, Query, Arg } from 'type-graphql';
-
-import { ObjectIdScalar } from '../utils/object-id.scalar';
+import * as _ from 'lodash';
+import { UserInputError } from 'apollo-server';
+import { Resolver, Query, Arg, Args, Info } from 'type-graphql';
+import { graphqlMongodbProjection } from 'graphql-mongodb-projection';
 
 import { User, UserModel } from '../entities/user';
 
 @Resolver(of => User)
 export class UserResolver {
 
-    @Query(returns => User, { nullable: true })
-    user(@Arg('userId', type => ObjectIdScalar) userId: ObjectId) {
-        return UserModel.findById(userId);
+    @Query(() => User)
+    public async user(
+        @Arg("id", { nullable: false })
+        id: string,
+
+        @Info()
+        info: any,
+    ): Promise<User> {
+        const item = await UserModel.findById(id).select(graphqlMongodbProjection(info));
+
+        if (_.isNil(item)) {
+            throw new UserInputError(`${id} not found`);
+        }
+
+        return item;
     }
 
     @Query(returns => [User])
