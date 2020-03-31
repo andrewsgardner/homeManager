051d4d1c8c75c82e51480297d6792fe290e14b7b
Andrew S. Gardner <asg@andrewsgardner.com>
2020-03-30 20:57:02 -0400

    Add IUser interface

---
+++ ui-service/src/app/models/user.ts
@@ -0,0 +1,9 @@
+export interface IUser {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    nickname?: string;
+    createdDate: string;
+    updatedDate: string;
+}
