da113c27321cfb65628fb74f097d9252c48dc396
Andrew S. Gardner <asg@andrewsgardner.com>
2020-06-28 16:46:38 -0400

    Test requiredGroups

--- ui-service/src/app/app-routing.module.ts
+++ ui-service/src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CanAuthenticationGuard } from './guards/keycloak-auth.guard';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component'; // TODO: remove when real routing is established
+
+
+const routes: Routes = [
+  { path: '', component: AppComponent, canActivate: [CanAuthenticationGuard], data: { requiredGroups: ['hm-user'] } }, // TODO: remove when real routing is established
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {  
+    scrollPositionRestoration: 'enabled'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
