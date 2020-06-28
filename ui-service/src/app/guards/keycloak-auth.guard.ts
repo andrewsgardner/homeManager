389ce84df5042c25d5cc621cf02444db9cb82f2c
Andrew S. Gardner <asg@andrewsgardner.com>
2020-06-28 16:47:17 -0400

    Add Keycloak access methods

--- ui-service/src/app/guards/keycloak-auth.guard.ts
+++ ui-service/src/app/guards/keycloak-auth.guard.ts
@@ -1,32 +1,62 @@
+import * as _ from 'lodash';
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { take } from 'rxjs/operators';
 import { KeycloakAuthGuard, KeycloakService } from 'keycloak-angular';
+
+import { AuthService } from '../services/auth.service';
+import { ConfigService } from '../services/config.service';
+import { IUserAccess } from '../models/user-access.interface';
  
 @Injectable({
   providedIn: 'root'
 })
-export class CanAuthenticationGuard extends KeycloakAuthGuard implements CanActivate {
-  constructor(protected router: Router, protected keycloakAngular: KeycloakService) {
+export class CanAuthenticationGuard extends KeycloakAuthGuard {
+  constructor(protected router: Router,
+              protected keycloakAngular: KeycloakService,
+              protected authService: AuthService,
+              protected configService: ConfigService) {
     super(router, keycloakAngular);
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
+    if (this.configService.useKeycloak()) {
+      return super.canActivate(route, state) as Promise<boolean>;
+    } else {
+      return new Promise((resolve, reject) => {
+        resolve(true);
+      });
+    }
   }
  
   isAccessAllowed(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
     return new Promise((resolve, reject) => {
+
+      if (!this.configService.useKeycloak()) {
+        resolve(true);
+      }
+
       if (!this.authenticated) {
-        this.keycloakAngular.login()
-          .catch(e => console.error(e));
-        return reject(false);
+        console.info('Logging into Keycloak...');
+        this.authService.lastLoginFlag = false;
+        this.keycloakAngular.login();
+        return;
       }
- 
-      const requiredRoles: string[] = route.data.roles;
-      if (!requiredRoles || requiredRoles.length === 0) {
-        return resolve(true);
-      } else {
-        if (!this.roles || this.roles.length === 0) {
-          resolve(false);
+
+      this.authService.updateLastLogin().pipe(take(1)).subscribe();
+
+      this.authService.getUserAccess().pipe(take(1)).subscribe((access: IUserAccess) => {
+
+        if (_.isArray(route.data.requiredGroups) && _.some(route.data.requiredGroups)) {
+          if (!route.data.requiredGroups.every((group) => access.groups.indexOf(group) > -1)) {
+            resolve(false);
+            return;
+          }
+
+          resolve(true);
         }
-        resolve(requiredRoles.every(role => this.roles.indexOf(role) > -1));
-      }
+
+      });
     });
   }
 }
--- ui-service/src/app/services/auth.service.ts
+++ ui-service/src/app/services/auth.service.ts
@@ -1,9 +1,110 @@
+import * as _ from 'lodash';
 import { Injectable } from '@angular/core';
+import { Observable, from, of } from 'rxjs';
+import { filter, switchMap, map, tap } from 'rxjs/operators';
+import { KeycloakService } from 'keycloak-angular';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+
+import { ConfigService } from './config.service';
+import { IUserProfile } from '../models/user-profile.interface';
+import { IUserAccess } from '../models/user-access.interface';
+import { IUser } from '../models/user.interface';
+import { ApolloQueryResult } from 'apollo-client';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  public lastLoginFlag: boolean = false;
+
+  constructor(private configService: ConfigService,
+              private keycloakService: KeycloakService,
+              private apollo: Apollo) { }
+
+  public isLoggedIn(): Observable<boolean> {
+    return from(this.keycloakService.isLoggedIn());
+  }
+
+  public userProfile(forceReload?: boolean): Observable<IUserProfile> {
+    if (this.configService.useKeycloak()) {
+      return from(this.keycloakService.isLoggedIn()).pipe(
+        filter((f: boolean) => f === true),
+        switchMap(() => from(this.keycloakService.loadUserProfile(forceReload)).pipe(
+          map((kcProfile: Keycloak.KeycloakProfile) => ({
+            ...kcProfile,
+            systemUser: false
+          }))
+        )),
+      );
+    } else {
+      return of({
+        id: '0123456789',
+        username: 'systemuser',
+        firstName: 'John',
+        lastName: 'Smith',
+        enabled: true,
+        emailVerified: true,
+        totp: true,
+        createdTimestamp: 0,
+        systemUser: true
+      });
+    }
+  }
+
+  public logout() {
+    if (this.configService.useKeycloak()) {
+      this.keycloakService.logout();
+    }
+  }
+
+  public updateLastLogin(): Observable<IUser> {
+    if (this.lastLoginFlag) {
+      return of(null);
+    }
+
+    const mutation = gql`
+      mutation {
+        login {
+          id
+          lastLoginDate
+        }
+      }
+    `;
+
+    return this.apollo.mutate<IUser>({
+      mutation
+    }).pipe(
+      tap((data: ApolloQueryResult<IUser>) => {
+        console.log('[Apollo login] ', data);
+        this.lastLoginFlag = true;
+      }),
+      map((data: ApolloQueryResult<IUser>) => {
+        return data.data;
+      })
+    )
+  }
+
+  public getUserAccess(): Observable<IUserAccess>{
+    const jwDecode = require('jwt-decode');
+    
+    return from(this.keycloakService.getToken()).pipe(
+      map((rawToken: string) => {
+        let tokenContent: any;
+
+        try {
+          tokenContent = jwDecode(rawToken);
+        } catch {
+          return {
+            groups: []
+          };
+        }
+
+        const groups: string[] = (_.isArray(tokenContent.groups)) ? tokenContent.groups.map((g: string) => g.substr(1)) : [];
+
+        return { groups };
+      })
+    )
+  }
 }
