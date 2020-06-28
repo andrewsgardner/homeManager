7fe685abcbb289924db737fe55a56e211e94fad3
Andrew S. Gardner <asg@andrewsgardner.com>
2020-06-28 16:50:34 -0400

    Update UserEntity

--- graphql-service/src/entities/user.entity.ts
+++ graphql-service/src/entities/user.entity.ts
@@ -2,7 +2,7 @@ import { getModelForClass, prop } from '@typegoose/typegoose';
 import { Field, ObjectType } from 'type-graphql';
 
 @ObjectType()
-export class User {
+export class UserEntity {
 
     @prop()
     @Field({ name: "id" })
@@ -42,4 +42,9 @@ export class User {
 
 }
 
-export const UserModel = getModelForClass(User);
+export const UserModel = getModelForClass(UserEntity, {
+    schemaOptions: {
+        collection: "users",
+        versionKey: "version"
+    }
+});
