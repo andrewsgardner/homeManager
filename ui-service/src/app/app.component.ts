84c3a0c95407b2a44d929804f6b4420cd5c4f034
Andrew S. Gardner <asg@andrewsgardner.com>
2020-06-28 16:45:43 -0400

    Test keycloak profile settings

--- ui-service/src/app/app.component.ts
+++ ui-service/src/app/app.component.ts
@@ -1,22 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
 import { UserService } from './services/user.service';
-import { IGqlResponseEnvelope } from './models/gql-response-envelope';
+import { IGqlResponseEnvelope } from './models/gql-response-envelope.interface';
 import { take } from 'rxjs/operators';
-import { IUser } from './models/user';
+import { IUser } from './models/user.interface';
+import { IUserProfile } from './models/user-profile.interface';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'homeManager';
   
-  constructor(private userService: UserService) {
+  public userProfile$: Observable<IUserProfile>;
+  
+  constructor(private authService: AuthService,
+              private userService: UserService) {
     /*
-    this.userService.getUser('51596c6e-d1a7-447c-abb2-7554a7b1e01b').pipe(take(1)).subscribe((res: IGqlResponseEnvelope<IUser>) => {
+    this.userService.getUser('f3f4ea6a-61ef-460b-b810-e7d11c3457ef').pipe(take(1)).subscribe((res: IGqlResponseEnvelope<IUser>) => {
       console.log(res);
     });
     */
+  }
+
+  ngOnInit() {
+    this.authService.isLoggedIn().subscribe((isLoggedIn: boolean) => {
+      if (!isLoggedIn) {
+        return;
+      }
+
+      this.userProfile$ = this.authService.userProfile();
+    });
   }
 }
